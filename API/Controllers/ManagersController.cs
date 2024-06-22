a4f4554af18390be8b2951c023661e345fa1a12a
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-06-22 17:33:35 -0300

    Added POST, PUT and DELETE Endpoints for the Manager Controller

--- API/Controllers/ManagersController.cs
+++ API/Controllers/ManagersController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
             return await _context.Managers.ToListAsync();
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetManager")]
         public async Task<ActionResult<Manager>> GetManager(int id)
         {
             var manager = await _context.Managers.FindAsync(id);
@@ -31,6 +31,51 @@ namespace API.Controllers
             }
 
             return manager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Manager>> CreateManager(Manager manager)
+        {
+            _context.Managers.Add(manager);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtAction("GetManager", new { Id = manager.ManagerId }, manager);
+
+            return BadRequest(new ProblemDetails { Title = "Problem creating new manager" });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateManager(Manager manager)
+        {
+            var managerEntity = await _context.Managers.FindAsync(manager.ManagerId);
+
+            if (managerEntity == null) return NotFound();
+
+            managerEntity.ManagerName = manager.ManagerName;
+            managerEntity.ManagerPhoneNumber = manager.ManagerPhoneNumber;
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok(managerEntity);
+
+            return BadRequest(new ProblemDetails { Title = "Problem updating manager" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteManager(int id)
+        {
+            var manager = await _context.Managers.FindAsync(id);
+
+            if (manager == null) return NotFound();
+
+            _context.Managers.Remove(manager);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Problem deleting manager" });
         }
     }
 }
